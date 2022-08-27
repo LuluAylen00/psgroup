6c63b99cfa64dea5773f8cd7e980c30298292f07
Aylen <luluaylen00@gmail.com>
2022-08-27 16:08:26 -0300

    Continuación del desarrollo de la página (home)

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const {resolve} = require('path');
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.listen(port);
+
+const public = resolve(__dirname, '../public');
+const static = express.static(public);
+app.use(static);
+
+app.get('/', (req, res) => {
+  let view = resolve(__dirname, 'views','index.html');
+  return res.sendFile(view);
+}) 
